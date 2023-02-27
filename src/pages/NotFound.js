9bb3c53de6d39b4aa109bac0536ba2e4c8d5729d
Adrian Suzdorf <suzdorfa@gmail.com>
2023-02-27 21:38:26 +0100

    Error page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,16 +5,18 @@ import unown from "../images/unown.png";
 
 const NotFound = () => {
   return (
-    <div>
+    <div className="error">
       <h1>Page not found</h1>
-      <figure>
+      <figure className="error__figure u-shadow">
         <img width={100} height={100} src={unown} alt="" />
-        <figcaption>
-          Unown - very cool Pokemon which has many forms. Every form presents
-          one of the alphabet's character
+        <figcaption className="error__figcaption">
+          <strong>Unown</strong> - cool Pokemon which has many forms. Every form
+          presents one of the alphabet's character.
         </figcaption>
       </figure>
-      <Link to="/">Back</Link>
+      <Link className="btn" to="/">
+        Back
+      </Link>
     </div>
   );
 };
